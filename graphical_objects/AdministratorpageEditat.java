b9221e669a0735eb2b768e15e8f7d045112a34e6
Alderon037 <48827672+Alderon037@users.noreply.github.com>
2020-06-15 12:22:21 +0300

    Update AdministratorpageEditat.java

--- graphical_objects/AdministratorpageEditat.java
+++ graphical_objects/AdministratorpageEditat.java
@@ -45,15 +45,15 @@ public class AdministratorPage extends JFrame{
         JFrame stergefilme = new JFrame("Delete movie");
         stergefilme.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         stergefilme.setLocation(400, 200);
-	//JFrame adaugacamere = new JFrame("Add room");
-	//adaugacamere.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-	//adaugacamere.setLocation(400, 200);
-	//JFrame stergecamere = new JFrame("Delete room");
-	//stergecamere.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-	//stergecamere.setLocation(400, 200);
-	//JFrame modificabilete = new JFrame("Modify Tickets");
-	//modificabilete.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-	//modificabilete.setLocation(400, 200);
+	JFrame adaugacamere = new JFrame("Add room");
+	adaugacamere.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+	adaugacamere.setLocation(400, 200);
+	JFrame stergecamere = new JFrame("Delete room");
+	stergecamere.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+	stergecamere.setLocation(400, 200);
+	JFrame modificabilete = new JFrame("Modify Tickets");
+	modificabilete.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+	modificabilete.setLocation(400, 200);
 
         
         bfilme.addActionListener(new ActionListener() { //add movies
@@ -203,183 +203,183 @@ public class AdministratorPage extends JFrame{
         	}
             
         });
-//        acamere.addActionListener(new ActionListener(){
-//		
-//		public void actionPerformed(ActionEvent e){
-//
-//			adaugacamere.setContentPane(new JPanel());
-//			adaugacamere.setSize(500,500);
-//			GridBagLayout layout = new GridBagLayout();
-//			adaugacamere.getContentPane().setLayout(layout);
-//			adaugacamere.setVisible(true);
-//
-//            		JLabel label_1 = new JLabel("Randuri: ");
-//            		JLabel label_2 = new JLabel("Coloane: ");
-//
-//            		JTextField randuri = new JTextField(25);
-//            		JTextField coloane = new JTextField(15);
-//
-//            		label_1.setLabelFor(randuri);
-//            		label_2.setLabelFor(coloane);
-//            
-//                	GridBagConstraints c = new GridBagConstraints();
-//			c.fill = GridBagConstraints.VERTICAL;
-//			c.gridx = 0;
-//			c.gridy = 0;
-//			modificabilete.add(label_1, c);
-//
-//			c.gridx = 1;
-//			c.gridy = 0;
-//			modificabilete.add(randuri, c);
-//
-//			c.gridx = 0;
-//			c.gridy = 1;
-//			modificabilete.add(label_2, c);
-//
-//			c.gridx = 1;
-//			c.gridy = 1;
-//			modificabilete.add(coloane, c);
-//			
-//			c.gridx = 0;
-//			c.gridy = 2;
-//			c.gridwith = 2;
-//			modificabilete.add(adauga, c);
-//			adauga.addActionListener(new ActionListener() {  
-//        			public void actionPerformed(ActionEvent e) {
-//        				ArrayList<Room> list = new ArrayList<Room>();
-//					list.add(new Room(randuri.getText(),coloane.getText()));
-//					adaugacamere.dispose();
-//        				JOptionPane.showMessageDialog(adaugacamere, "The room has been added succesfully!");
-//			}
-//      	});
-//
-//      } 
-//
-//});
-//        bcamere.addActionListener(new ActionListener(){
-//		
-//		public void actionPerformed(ActionEvent e){
-//
-//			stergecamere.setContentPane(new JPanel());
-//			stergecamere.setSize(500,500);
-//			GridBagLayout layout = new GridBagLayout();
-//			stergecamere.getContentPane().setLayout(layout);
-//			stergecamere.setVisible(true);
-//
-//			JLabel label_1 = new JLabel("ID: ");
-//
-//			JTextField id = new JTextField(15);
-//      
-//			label_1.setLabelFor(id);
-//            	
-//                	GridBagConstraints c = new GridBagConstraints();
-//			c.fill = GridBagConstraints.VERTICAL;
-//			c.gridx = 0;
-//			c.gridy = 0;
-//			stergecamere.add(label_1, c);
-//
-//			c.gridx = 1;
-//			c.gridy = 0;
-//			stergecamere.add(id, c);
-//			
-//			c.gridx = 0;
-//			c.gridy = 1;
-//			c.gridwith = 2;
-//			stergecamere.add(sterge, c);
-//			sterge.addActionListener(new ActionListener(){
-//				public void actionPerformed(ActionEvent e){
-//				ArrayList<Room> camere = Utility.read_from_file("data/room.csv", Room.class);
-//                       	String name = id.getText();
-//  		            	for (Room it: camere) {
-//  		            	
-//  		                 	if(it.getId().equals(name)) {
-//  		                        	camere.remove(it);
-//  		                        	try {
-//										FileWriter writer = new FileWriter("data/room.csv"); //deleting the content of the file
-//										Utility.write_to_file("data/room.csv", camere);
-//										JOptionPane.showMessageDialog(stergefilme, "The room was deleted from the list..");
-//	    		                        stergecamere.dispose();
-//	    		                        writer.close();
-//	    		                        return;
-//									} catch (IOException e1) {
-//										// TODO Auto-generated catch block
-//										e1.printStackTrace();
-//									}
-//  		                       
-//    		                 }
-//  		            }
-//   		            stergecamere.dispose();
-//    		            JOptionPane.showMessageDialog(stergecamere, "The room was not found!");
-//                   }  
-//               });
-//      	}
-//          
-//      });
-//        abilete.addActionListener(new ActionListener(){
-//		
-//		public void actionPerformed(ActionEvent e){
-//
-//			modificabilete.setContentPane(new JPanel());
-//			modificabilete.setSize(500,500);
-//			GridBagLayout layout = new GridBagLayout();
-//			modificabilete.getContentPane().setLayout(layout);
-//			modificabilete.setVisible(true);
-//
-//			JLabel label_1 = new JLabel("ID: ");
-//            		JLabel label_2 = new JLabel("Randuri: ");
-//            		JLabel label_3 = new JLabel("Coloane: ");
-//
-//
-//			JTextField id = new JTextField(15);
-//            		JTextField randuri = new JTextField(25);
-//            		JTextField coloane = new JTextField(15);
-//
-//			label_1.setLabelFor(id);
-//            		label_2.setLabelFor(randuri);
-//            		label_3.setLabelFor(coloane);
-//            
-//                	GridBagConstraints c = new GridBagConstraints();
-//			c.fill = GridBagConstraints.VERTICAL;
-//			c.gridx = 0;
-//			c.gridy = 0;
-//			modificabilete.add(label_1, c);
-//
-//			c.gridx = 1;
-//			c.gridy = 0;
-//			modificabilete.add(id, c);
-//
-//			c.gridx = 0;
-//			c.gridy = 1;
-//			modificabilete.add(label_2, c);
-//
-//			c.gridx = 1;
-//			c.gridy = 1;
-//			modificabilete.add(randuri, c);
-//
-//			c.gridx = 0;
-//			c.gridy = 2;
-//			modificabilete.add(label_3, c);
-//
-//			c.gridx = 1;
-//			c.gridy = 2;
-//			modificabilete.add(coloane, c);
-//			
-//			c.gridx = 0;
-//			c.gridy = 3;
-//			c.gridwith = 2;
-//			modificabilete.add(modifica, c);
-//			modifica.addActionListener(new ActionListener(){
-//				public void actionPerformed(ActionEvent e){
-//					ArrayList<OccupiedSeat> list = Utility.read_from_file("data/occupied_seats.csv", OccupiedSeat.class);
-//					String numar = id.getText();
-//					for(OccupiedSeat it: list)
-//						if(it.getRoom().equals(id))
-//					
-//                    new Room(randuri,coloane);
-//                }
-//            });
-//	}
-//});
+        acamere.addActionListener(new ActionListener(){
+		
+		public void actionPerformed(ActionEvent e){
+
+			adaugacamere.setContentPane(new JPanel());
+			adaugacamere.setSize(500,500);
+			GridBagLayout layout = new GridBagLayout();
+			adaugacamere.getContentPane().setLayout(layout);
+			adaugacamere.setVisible(true);
+
+            		JLabel label_1 = new JLabel("Randuri: ");
+            		JLabel label_2 = new JLabel("Coloane: ");
+
+            		JTextField randuri = new JTextField(25);
+            		JTextField coloane = new JTextField(15);
+
+            		label_1.setLabelFor(randuri);
+            		label_2.setLabelFor(coloane);
+            
+                	GridBagConstraints c = new GridBagConstraints();
+			c.fill = GridBagConstraints.VERTICAL;
+			c.gridx = 0;
+			c.gridy = 0;
+			modificabilete.add(label_1, c);
+
+			c.gridx = 1;
+			c.gridy = 0;
+			modificabilete.add(randuri, c);
+
+			c.gridx = 0;
+			c.gridy = 1;
+			modificabilete.add(label_2, c);
+
+			c.gridx = 1;
+			c.gridy = 1;
+			modificabilete.add(coloane, c);
+			
+			c.gridx = 0;
+			c.gridy = 2;
+			c.gridwith = 2;
+			modificabilete.add(adauga, c);
+			adauga.addActionListener(new ActionListener() {  
+        			public void actionPerformed(ActionEvent e) {
+        				ArrayList<Room> list = new ArrayList<Room>();
+					list.add(new Room(randuri.getText(),coloane.getText()));
+					adaugacamere.dispose();
+        				JOptionPane.showMessageDialog(adaugacamere, "The room has been added succesfully!");
+			}
+      		});
+
+     	 } 
+
+});
+        bcamere.addActionListener(new ActionListener(){
+		
+		public void actionPerformed(ActionEvent e){
+
+			stergecamere.setContentPane(new JPanel());
+			stergecamere.setSize(500,500);
+			GridBagLayout layout = new GridBagLayout();
+			stergecamere.getContentPane().setLayout(layout);
+			stergecamere.setVisible(true);
+
+			JLabel label_1 = new JLabel("ID: ");
+
+			JTextField id = new JTextField(15);
+      
+			label_1.setLabelFor(id);
+            	
+                	GridBagConstraints c = new GridBagConstraints();
+			c.fill = GridBagConstraints.VERTICAL;
+			c.gridx = 0;
+			c.gridy = 0;
+			stergecamere.add(label_1, c);
+
+			c.gridx = 1;
+			c.gridy = 0;
+			stergecamere.add(id, c);
+			
+			c.gridx = 0;
+			c.gridy = 1;
+			c.gridwith = 2;
+			stergecamere.add(sterge, c);
+			sterge.addActionListener(new ActionListener(){
+				public void actionPerformed(ActionEvent e){
+				ArrayList<Room> camere = Utility.read_from_file("data/room.csv", Room.class);
+                       		String name = id.getText();
+  		            	for (Room it: camere) {
+  		            	
+  		                 	if(it.getId().equals(name)) {
+  		                        	camere.remove(it);
+  		                        	try {
+										FileWriter writer = new FileWriter("data/room.csv"); //deleting the content of the file
+										Utility.write_to_file("data/room.csv", camere);
+										JOptionPane.showMessageDialog(stergefilme, "The room was deleted from the list..");
+	    		                        stergecamere.dispose();
+	    		                        writer.close();
+	    		                        return;
+									} catch (IOException e1) {
+										// TODO Auto-generated catch block
+										e1.printStackTrace();
+									}
+  		                       
+    		                 }
+  		            }
+   		            stergecamere.dispose();
+    		            JOptionPane.showMessageDialog(stergecamere, "The room was not found!");
+                   }  
+               });
+      	}
+          
+      });
+        abilete.addActionListener(new ActionListener(){
+		
+		public void actionPerformed(ActionEvent e){
+
+			modificabilete.setContentPane(new JPanel());
+			modificabilete.setSize(500,500);
+			GridBagLayout layout = new GridBagLayout();
+			modificabilete.getContentPane().setLayout(layout);
+			modificabilete.setVisible(true);
+
+			JLabel label_1 = new JLabel("ID: ");
+            		JLabel label_2 = new JLabel("Randuri: ");
+            		JLabel label_3 = new JLabel("Coloane: ");
+
+
+			JTextField id = new JTextField(15);
+            		JTextField randuri = new JTextField(25);
+            		JTextField coloane = new JTextField(15);
+
+			label_1.setLabelFor(id);
+            		label_2.setLabelFor(randuri);
+            		label_3.setLabelFor(coloane);
+            
+                	GridBagConstraints c = new GridBagConstraints();
+			c.fill = GridBagConstraints.VERTICAL;
+			c.gridx = 0;
+			c.gridy = 0;
+			modificabilete.add(label_1, c);
+
+			c.gridx = 1;
+			c.gridy = 0;
+			modificabilete.add(id, c);
+
+			c.gridx = 0;
+			c.gridy = 1;
+			modificabilete.add(label_2, c);
+
+			c.gridx = 1;
+			c.gridy = 1;
+			modificabilete.add(randuri, c);
+
+			c.gridx = 0;
+			c.gridy = 2;
+			modificabilete.add(label_3, c);
+
+			c.gridx = 1;
+			c.gridy = 2;
+			modificabilete.add(coloane, c);
+			
+			c.gridx = 0;
+			c.gridy = 3;
+			c.gridwith = 2;
+			modificabilete.add(modifica, c);
+			modifica.addActionListener(new ActionListener(){
+				public void actionPerformed(ActionEvent e){
+					ArrayList<OccupiedSeat> list = Utility.read_from_file("data/occupied_seats.csv", OccupiedSeat.class);
+					String numar = id.getText();
+					for(OccupiedSeat it: list)
+						if(it.getRoom().equals(id))
+					
+                    new Room(randuri,coloane);
+                }
+            });
+	  }
+	});
     }
 	
 }
